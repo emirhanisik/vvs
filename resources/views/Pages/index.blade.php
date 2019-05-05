d1a82078b464c43d32172a635c495ab78b17bb92
bizmirli <berkay.izmirli@gmail.com>
2019-05-05 14:57:28 +0300

    display heading

--- resources/views/Pages/index.blade.php
+++ resources/views/Pages/index.blade.php
@@ -2,7 +2,7 @@
 
 @section('content')
     <div class="jumbotron text-center">
-        <h1>Keşfet ve Anlat</h1>
+        <h1 class="display-1">Keşfet ve Anlat</h1>
         <p>Veni Vidi Scripsi Online Paylaşım Platformu</p>
         <p><a class="btn btn-primary btn-lg" href="/login" role="button" >Giriş Yap</a> <a class="btn btn-primary btn-lg" href="/register" role="button">Kayıt Ol</a></p>
     </div>
